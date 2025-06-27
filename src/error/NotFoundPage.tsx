9294608a1f6e7adcb4bd65e9efd7a03654f5066f
Renato Morales <104334139+naatox@users.noreply.github.com>
2025-06-27 18:42:09 -0400

    feat: refactor NotFoundPage layout for improved styling and structure

--- src/error/NotFoundPage.tsx
+++ src/error/NotFoundPage.tsx
@@ -10,8 +10,7 @@ const NotFoundPage = () => {
       className="min-h-screen flex items-center justify-center bg-cover bg-center"
       style={{ backgroundImage: "url('/img/image3.jpg')" }}
     >
-      <div className="bg-white bg-opacity-80 rounded-xl p-4 shadow-2xl">
-        <Card className="w-full max-w-md shadow-xl bg-transparent">
+        <Card className="max-w-md bg-opacity-80 bg-white p-6 shadow-2xl ">
           <CardHeader>
             <CardTitle className="text-3xl text-center text-red-500">404</CardTitle>
             <CardDescription className="text-center">
@@ -25,7 +24,6 @@ const NotFoundPage = () => {
             </Button>
           </CardContent>
         </Card>
-      </div>
     </div>
   );
 };
